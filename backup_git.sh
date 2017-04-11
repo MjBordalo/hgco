dd70b003a61cea7bc991e7410506543a0bbb211f
MjBordalo <miguel.b.fernandes@gmail.com>
2017-04-11 18:37:59 +0100

    add scrpit para backup automatico

---
+++ backup_git.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "running backup on git"
+
+sudo git add-A
+sudo git commit -m
+sudo git push
---
+++ mysh.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Hello woel"
+
+sudo git add -A
+sudo git commit
+sudo git push
